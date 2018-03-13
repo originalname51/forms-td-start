b3a60289508e4a18acd5c6ec9d5c3d5372f1186a
Robert Stine <robwstine@outlook.com>
2018-03-12 21:47:19 -0400

    Template form final commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,8 +11,33 @@ export class AppComponent {
   @ViewChild('f') signUpForm: NgForm;
   defaultSelectValue = 'pet';
   answer = '';
+  genders = ['male', 'female'];
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  };
+  private submitted = false;
+
   suggestUserName() {
     const suggestedName = 'Superuser';
+    /* set exact copy - not good approach but can control everything.
+    this.signUpForm.setValue({
+      userData: {
+        username: suggestedName,
+        email: ''
+      },
+      secret: 'pet',
+      questionAnswer: '',
+      gender: 'male'
+    }); */
+
+    // Ideal way to change specific values.
+   this.signUpForm.form.patchValue({userData: {
+     username: suggestedName
+     }});
   }
 
   /*  onSubmit(form: NgForm) {
@@ -20,6 +45,14 @@ export class AppComponent {
     }*/
 
   onSubmit() {
+    this.submitted = true;
     console.log(this.signUpForm);
+    this.user.username = this.signUpForm.value.userData.username;
+    this.user.email = this.signUpForm.value.userData.email;
+    this.user.secretQuestion = this.signUpForm.value.secret;
+    this.user.answer = this.signUpForm.value.questionAnswer;
+    this.user.gender = this.signUpForm.value.gender;
+
+    this.signUpForm.reset();
   }
 }
